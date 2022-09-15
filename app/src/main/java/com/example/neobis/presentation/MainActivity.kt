f834001f47252579084c7b39a7f17ea7f68c9c51
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-09-15 19:55:27 +0600

    Added Domain layer

--- app/src/main/java/com/example/neobis/presentation/MainActivity.kt
+++ app/src/main/java/com/example/neobis/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.neobis
+package com.example.neobis.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.neobis.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
